2a6756e92608217edb0c48a7cd4a8071c86955ef
AlexPoddubny <usatij.alex@gmail.com>
2019-12-19 17:39:30 +0200

    Form dropdownlist

--- modules/admin/views/category/_form.php
+++ modules/admin/views/category/_form.php
@@ -1,7 +1,9 @@
 <?php
-
-use yii\helpers\Html;
-use yii\widgets\ActiveForm;
+	
+	use app\modules\admin\models\Category;
+	use yii\helpers\ArrayHelper;
+	use yii\helpers\Html;
+	use yii\widgets\ActiveForm;
 
 /* @var $this yii\web\View */
 /* @var $model app\modules\admin\models\Category */
@@ -13,6 +15,9 @@ use yii\widgets\ActiveForm;
     <?php $form = ActiveForm::begin(); ?>
 
     <?php //echo $form->field($model, 'parent_id')->textInput() ?>
+    <?= $form->field($model, 'parent_id')
+	    ->dropDownList(ArrayHelper::map(Category::find()->all(), 'id', 'name'));
+    ?>
 
     <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
 
